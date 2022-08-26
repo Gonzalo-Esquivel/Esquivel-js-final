670b3eb3a261a00ecfb9cc87c9b535d38b37aa46
Gonzalo Esquivel <gesquivel263@gmail.com>
2022-08-25 22:56:04 -0300

    operador ternario y spread

---
+++ js/usuarios.js
@@ -0,0 +1,12 @@
+const usuario = {
+    nombre: 'Gonzalo',
+    id:1422
+}
+
+let copiaUsuario = {...usuario}
+
+copiaUsuario.web = 'instagram.com/gonzalo95'
+
+console.log ({usuario})
+console.log ({copiaUsuario})
+
